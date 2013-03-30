4b4e5e92bacc315960b741be849b795506c6302f
Dylan Lloyd <dylan@dylansserver.com>
2013-03-30 12:26:36 -0400

    don't try to parse if a number isn't given

--- lib/App/GitHubPullRequest.pm
+++ lib/App/GitHubPullRequest.pm
@@ -148,11 +148,15 @@ added and the branch in question will be fetched.
 
 sub checkout {
     my ($self, $number) = @_;
-    $number =~ s{[^\d]}{}g;
-    die("Please specify a pull request number.\n") unless $number;
-    my $pr = $self->_fetch_one($number);
-    die("Unable to fetch pull request $number.\n")
-        unless defined $pr;
+    my $pr;
+    $number =~ s{[^\d]}{}g if defined $number;
+    if ($number) {
+        $pr = $self->_fetch_one($number);
+        die("Unable to fetch pull request $number.\n")
+            unless defined $pr;
+    } else {
+        die("Please specify a pull request number.\n");
+    }
 
     # Get required contributor branch info
     my $head_repo   = $pr->{'head'}->{'repo'}->{'git_url'};
