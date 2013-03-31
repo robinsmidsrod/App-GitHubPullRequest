6288fe85cd86a6a89b49403fde04d767b51e0a55
Robin Smidsrød <robin@smidsrod.no>
2013-03-31 09:54:37 +0200

    Cleaned up logic in checkout command - Guard clauses FTW

--- lib/App/GitHubPullRequest.pm
+++ lib/App/GitHubPullRequest.pm
@@ -161,15 +161,9 @@ added and the branch in question will be fetched.
 
 sub checkout {
     my ($self, $number) = @_;
-    my $pr;
     $number =~ s{[^\d]}{}g if defined $number;
-    if ($number) {
-        $pr = $self->_fetch_one($number);
-        die("Unable to fetch pull request $number.\n")
-            unless defined $pr;
-    } else {
-        die("Please specify a pull request number.\n");
-    }
+    die("Please specify a pull request number.\n") unless $number;
+    my $pr = $self->_fetch_one($number);
 
     # Get required contributor branch info
     my $head_repo   = $pr->{'head'}->{'repo'}->{'git_url'};
